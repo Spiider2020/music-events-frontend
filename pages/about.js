a1490f2763b43fe6ab911504843319c2de5dcd60
Catalin <Spiider11@yahoo.com>
2021-10-12 11:11:08 +0300

    some minor changes

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,12 @@
+import Link from 'next/link';
+
 export default function AboutPage() {
 	return (
 		<div>
 			<h1>About page</h1>
 			<p>This is an app to find the latest musical events.</p>
 			<p>version: 1.0.0</p>
+			<Link href='/'>Home</Link>
 		</div>
 	);
 }
