efda4a6737b3782dd6b6dd0cdfdbbcc10566172d
Catalin <Spiider11@yahoo.com>
2021-10-13 15:18:40 +0300

    Layout, header and footer

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,11 @@
-import Link from 'next/link';
+import Layout from '../components/Layout';
 
 export default function AboutPage() {
 	return (
-		<div>
+		<Layout title='About Music Events'>
 			<h1>About page</h1>
 			<p>This is an app to find the latest musical events.</p>
 			<p>version: 1.0.0</p>
-			<Link href='/'>Home</Link>
-		</div>
+		</Layout>
 	);
 }
